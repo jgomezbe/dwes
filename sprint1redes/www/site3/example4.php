250615aa81483e76d3c7159d6afe0ab6fc0bba25
Javier Gómez <jgomezb@fpcoruna.afundacion.org>
2022-09-29 15:29:45 +0200

    modificado example4

--- sprint1redes/www/site3/example4.php
+++ sprint1redes/www/site3/example4.php
@@ -5,11 +5,13 @@
 	function edad_en_5_años($edad){
 	return $edad + 5;
 }
-if (edad_en_5_años($_GET[edad])>65){
-	echo "En 5 años tendrás edad de jubilación";
-	}else {
-	echo "¡Disfruta de tu tiempo!";
+	function mensaje($age){
+	if(edad_en_5_años ($age) > 65){
+	return "En 10 años tendrás edad de jubilación";
+} else{
+	return "¡Disfruta de tu tiempo!";
 	}
+}
 ?>
 <table>
 <tr>
@@ -18,10 +20,10 @@ if (edad_en_5_años($_GET[edad])>65){
 </tr>
 <?php
 	$lista =array(64,65,61,59,53);
-foreach($lista as $valor){
+foreach ($lista as $valor){
 	echo "<tr>";
 	echo "<td>".$valor."</td>";
-	echo "<td".mensaje($valor)."</td>";
+	echo "<td>".mensaje($valor)."</td>";
 	echo "</tr>";
 	}
 ?>
