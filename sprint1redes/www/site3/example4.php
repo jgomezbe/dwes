326e7817e349c37b284edde5a22dda946bd703f8
Javier Gómez <jgomezb@fpcoruna.afundacion.org>
2022-09-29 12:23:23 +0200

    modificado example4

--- sprint1redes/www/site3/example4.php
+++ sprint1redes/www/site3/example4.php
@@ -11,5 +11,20 @@ if (edad_en_5_años($_GET[edad])>65){
 	echo "¡Disfruta de tu tiempo!";
 	}
 ?>
+<table>
+<tr>
+<th>Edad</th>
+<th>Info</th>
+</tr>
+<?php
+	$lista =array(64,65,61,59,53);
+foreach($lista as $valor){
+	echo "<tr>";
+	echo "<td>".$valor."</td>";
+	echo "<td".mensaje($valor)."</td>";
+	echo "</tr>";
+	}
+?>
+</table>
 </body>
 </html>
