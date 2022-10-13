abcaf3beb258377334a33e27a2413df6488013f5
Javier Gómez <jgomezb@fpcoruna.afundacion.org>
2022-10-13 11:47:30 +0200

    Terminada tarea 1.

---
+++ sprint2python/concurso.py
@@ -0,0 +1,16 @@
+
+
+print(" El mejor jugador de fútbol de la historia es: ")
+
+print(" Lionel Messi (a)")
+print(" Diego Armando Maradona (b)")
+print(" Cristiano Ronaldo (c) ")
+op = input('Elige sabiamente: ')
+if op == "a":
+    print(' Has fallado :(.')
+elif op == "b":
+    print(' Has fallado :( .')
+elif op == "c":
+    print(' Has acertado :) .')
+else:
+    print(" La opción tecleada es inválida.")
