7d0399eb878c58623e15a713056186151322fc40
Javier Gómez <jgomezb@fpcoruna.afundacion.org>
2022-10-17 13:24:23 +0200

    Terminada tarea 7.

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,64 +1,74 @@
-i = 0
-score = 0
+import random
+def preguntas(i):
+    score = 0
+    if i == 1:
+        print(" El mejor jugador de fútbol de la historia es: ")
+        print(" Lionel Messi (a)")
+        print(" Diego Armando Maradona (b)")
+        print(" Cristiano Ronaldo (c) ")
+        op = input('Elige: \n')
+        if op == "a":
+            print(' Has fallado :( .\n')
+            score -= 5
+            i += 1
+        elif op == "b":
+            print(' Has fallado :( .\n')
+            score -= 5
+            i += 1
+        elif op == "c":
+            print(' Has acertado :) .')
+            score += 10
+            i = 3
+        else:
+            print(" La opción tecleada es inválida.")
+    elif i == 2:
+        print(" El mejor tenista de la historia es: ")
+        print(" Novak Djokovic (a)")
+        print(" Rafa Nadal (b)")
+        print(" Roger Federer (c) ")
+        op = input('Elige: \n')
+        if op == "a":
+            print(' Has fallado :( .\n')
+            score -= 5
+            i += 1
+        elif op == "b":
+            print(' Has acertado :) .')
+            score += 10
+            i = 3
+        elif op == "c":
+            print(' Has fallado :( .\n')
+            score -= 5
+            i += 1
+        else:
+            print(" La opción tecleada es inválida.")
 
-print(" El mejor jugador de fútbol de la historia es: ")
-print(" Lionel Messi (a)")
-print(" Diego Armando Maradona (b)")
-print(" Cristiano Ronaldo (c) ")
-op = input('Elige: ')
-if op == "a":
-    print(' Has fallado :( .\n')
-    score -= 5
-    i += 1
-elif op == "b":
-    print(' Has fallado :( .\n')
-    score -= 5
-    i += 1
-elif op == "c":
-    print(' Has acertado :) .')
-    score += 10
-    i = 3
-else:
-    print(" La opción tecleada es inválida.")
+    elif i==3:
+        print(" El mejor jugador de la NBA de la historia es: ")
+        print(" Michael Jordan (a)")
+        print(" LeBron James (b)")
+        print(" Magic Johnson (c) ")
+        op = input('Elige: \n')
+        if op == "a":
+            print(' Has acertado :) .')
+            score += 10
+            i = 3
+        elif op == "b":
+            print(' Has fallado :( .\n')
+            score -= 5
+            i += 1
+        elif op == "c":
+            print(' Has fallado :( .\n')
+            score -= 5
+            i += 1
+        else:
+            print(" La opción tecleada es inválida.")
+    
+    return score    
 
-print(" El mejor tenista de la historia es: ")
-print(" Novak Djokovic (a)")
-print(" Rafa Nadal (b)")
-print(" Roger Federer (c) ")
-op = input('Elige: ')
-if op == "a":
-    print(' Has fallado :( .\n')
-    score -= 5
-    i += 1
-elif op == "b":
-    print(' Has acertado :) .')
-    score += 10
-    i = 3
-elif op == "c":
-    print(' Has fallado :( .\n')
-    score -= 5
-    i += 1
-else:
-    print(" La opción tecleada es inválida.")
-
-print(" El mejor jugador de ba de la historia es: ")
-print(" Michael Jordan (a)")
-print(" LeBron James (b)")
-print(" Magic Johnson (c) ")
-op = input('Elige: ')
-if op == "a":
-    print(' Has acertado :) .')
-    score += 10
-    i = 3
-elif op == "b":
-    print(' Has fallado :( .\n')
-    score -= 5
-    i += 1
-elif op == "c":
-    print(' Has fallado :( .\n')
-    score -= 5
-    i += 1
-else:
-    print(" La opción tecleada es inválida.")
-
-print(" \nHas obtenido "+str(score)+" puntos en "+str(i)+" intentos.")
+num1=random.randint(1,3)
+score1=preguntas(num1)
+num2=random.randint(1,3)
+while num1==num2:
+    num2=random.randint(1,3)
+score2=preguntas(num2)
+print(" \nHas obtenido "+str(score1+score2)+" puntos.")
--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -6,13 +6,21 @@ print('a) primer algoritmo')
 print('b) segundo algoritmo')
 op = input('Teclea una opción: ')
 if op == 'a':
+    start_time = time.time()
     n = int(input("Teclea un número: "))
     func_factorial(n)
     print(func_factorial(n))
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 
 elif op == 'b':
+    start_time = time.time()
     n = int(input("Teclea un número: "))
     func_factorial2(n)
+    end_time = time.time()
+    elapsed_time = end_time-start_time
     print(func_factorial2(n))
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
 else:
     print('La opción tecleada es inválida.')
