dab0405278b28782831e03f8d3aa515b59ea2557
Javier Gómez <jgomezb@fpcoruna.afundacion.org>
2022-10-17 12:52:49 +0200

    Terminada tarea 5.

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,4 +1,18 @@
 from factorial import func_factorial
-n = int(input("Teclea un número: "))
-func_factorial(n)
-print(func_factorial(n))
+from factorial2 import func_factorial2
+import time
+print('MENU')
+print('a) primer algoritmo')
+print('b) segundo algoritmo')
+op = input('Teclea una opción: ')
+if op == 'a':
+    n = int(input("Teclea un número: "))
+    func_factorial(n)
+    print(func_factorial(n))
+
+elif op == 'b':
+    n = int(input("Teclea un número: "))
+    func_factorial2(n)
+    print(func_factorial2(n))
+else:
+    print('La opción tecleada es inválida.')
