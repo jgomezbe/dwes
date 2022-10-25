254f8c3d86d38575314862a21fab6e3a178ef5a0
jgomezbe <113417205+jgomezbe@users.noreply.github.com>
2022-10-25 13:35:24 +0200

    Terminado detalle tarea 4, cambio detail.php para mostrar el usuario que comenta.

--- sprint3sesiones/www/mysite/detail.php
+++ sprint3sesiones/www/mysite/detail.php
@@ -24,9 +24,10 @@
 	<ul>
 	<?php
 	$query2= 'select * from tComentarios where libro_id='.$libro_id;
-	$result2= mysqli_query($db,$query2) or die ('Query error');
-	while ($row= mysqli_fetch_array($result2)){
-
+	$query3 = 'SELECT tUsuarios.nombre FROM tUsuarios,tComentarios WHERE tUsuarios.id=usuario_id and libro_id='.$libro_id;
+	$result2= mysqli_query($db, $query2) or die('Query error 2');
+	$result3= mysqli_query($db,$query3) or die('Query error 3');
+	while ($row = mysqli_fetch_array($result2)and $row2= mysqli_fetch_array($result3)) {
 		echo'<li>'.$row['comentario'].' '.$row['fecha_comentario'].'</li>';
 	}
 	mysqli_close($db);
@@ -35,8 +36,7 @@
 	<p>Deja un nuevo comentario:</p>
 	<form action="/comment.php" method="post">
 		<textarea rows="4" cols="50" name="new_comment"></textarea><br>
-		<input type="hidden" name="libro_id" value="
-		<?php echo $libro_id; ?>">
+		<input type="hidden" name="libro_id" value="<?php echo $libro_id; ?>">
 		<input type="submit" value="Comentar">
 	</form>
 	<a href="/logout.php">Logout</a>
