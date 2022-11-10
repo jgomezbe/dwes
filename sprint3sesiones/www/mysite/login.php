163a8fd46136527a3989308105afa0a7441f2f6d
Javier Gómez <jgomezb@fpcoruna.afundacion.org>
2022-11-10 10:46:03 +0100

    Terminados ejercicios opcionales.

--- sprint3sesiones/www/mysite/login.php
+++ sprint3sesiones/www/mysite/login.php
@@ -1,19 +1,19 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
-$email_posted = $_POST['f_email'];
-$password_posted = $_POST['f_password'];
-$query = "SELECT id, contraseña FROM tUsuarios WHERE email = '".$email_posted."'";
-$result = mysqli_query($db, $query) or die('Query error');
-if (mysqli_num_rows($result) > 0) {
-$only_row = mysqli_fetch_array($result);
-if ($only_row[1] == $password_posted) {
-session_start();
-$_SESSION['user_id'] = $only_row[0];
-header('Location: main.php');
-} else {
-echo '<p>Contraseña incorrecta</p>';
-}
-} else {
-echo '<p>Usuario no encontrado con ese email</p>';
-}
-?>
+	$db = mysqli_connect('172.16.0.2', 'root', '1234', 'mysitedb') or die('Fail');    
+	$email_posted = $_POST['f_email'];
+	$password_posted = $_POST['f_password'];
+	$query = "SELECT id, contraseña FROM tUsuarios WHERE email = '".$email_posted."'";
+	$result = mysqli_query($db, $query) or die('Query error');
+	if (mysqli_num_rows($result) > 0) {
+	$only_row = mysqli_fetch_array($result);
+	if ($only_row[1] == $password_posted) {
+	session_start();
+	$_SESSION['user_id'] = $only_row[0];
+	header('Location: main.php');
+	} else {
+	echo '<p>Contraseña incorrecta</p>';
+	}
+	} else {
+	echo '<p>Usuario no encontrado con ese email</p>';
+	}
+	?>
